8fb78cf2ae21187dbcbcc5c0965ec83a0c9ea901
Ilya Meignan Masson <imeignanmasson@fgrenoble>
2022-05-23 22:36:59 +0200

    expe_4 done

--- benchmarking/benchmark_colmet_rs.py
+++ benchmarking/benchmark_colmet_rs.py
@@ -49,16 +49,16 @@ def install_npb(hosts):
 def install_colmet(collector_host, hosts):
     logger.debug("== Installing Colmet(Rust version) == \n")
     #TODO : check if the user has kapack in his home directory
-    #command_node = "~/.nix-profile/bin/nix-env -f ~/nur-kapack -iA colmet-rs -I ~/.nix-defexpr/channels/"
-    #command_collector = "~/.nix-profile/bin/nix-env -f ~/nur-kapack -iA colmet-collector -I ~/.nix-defexpr/channels"
-    #p = Remote(command_node, hosts).start()
-    #c = SshProcess(command_collector, collector_host).run()
-    #p.wait()
-    c = Remote("~/.nix-profile/bin/nix-env -f ~/nur-kapack -iA colmet -I ~/.nix-defexpr/channels/", list(hosts, collector_host)).start()
+    command_node = "~/.nix-profile/bin/nix-env -f ~/nur-kapack -iA colmet-rs -I ~/.nix-defexpr/channels/"
+    command_collector = "~/.nix-profile/bin/nix-env -f ~/nur-kapack -iA colmet-collector -I ~/.nix-defexpr/channels"
+    p = Remote(command_node, hosts).start()
+    c = SshProcess(command_collector, collector_host).run()
+    p.wait()
+    #c = Remote("~/.nix-profile/bin/nix-env -f ~/nur-kapack -iA colmet -I ~/.nix-defexpr/channels/", hosts).start()
     c.wait()
     logger.debug(c.stdout)
-    #for k in p.processes:
-    #    logger.debug(k.stdout)
+    for k in p.processes:
+        logger.debug(k.stdout)
     print("Colmet installed : -- %s seconds --" % (time.time()-starttime))
 
 def parse_output(s):
@@ -83,8 +83,8 @@ class Colmet_bench(Engine):
     def start_colmet(self, collector_parameters, parameters):
         logger.debug("Start colmet node agent on all the compute nodes with the specified parameters and the collector on the corresponding host")
         command_node = ".nix-profile/bin/colmet-node --zeromq-uri tcp://{}:5556 {}".format(self.collector_hostname, parameters)
-        #command_collector = "~/.nix-profile/bin/colmet-collector"
-        command_collector = "~/.nix-profile/bin/colmet-collector {}".format(collector_parameters)
+        command_collector = "~/.nix-profile/bin/colmet-collector"
+        #command_collector = "~/.nix-profile/bin/colmet-collector {}".format(collector_parameters)
 
         self.colmet_nodes = Remote(command_node, self.hostnames).start()
         self.collector = SshProcess(command_collector, self.collector_hostname).start()
@@ -101,17 +101,13 @@ class Colmet_bench(Engine):
         self.colmet_launched = False
 
     def update_hostnames(self, nb_nodes):
-        restart_colmet=False
-        if nb_nodes != len(self.hostnames):
-            restart_colmet=True
         self.hostnames=list()
         hf=open("nodefile","w")
         for i in range(0, nb_nodes):
             self.hostnames.append(self.initial_hostnames[i])
-            for _ in range(0, int(get_host_attributes(self.initial_hostnames[i])['architecture']['nb_cores'])):
+            for _ in range(0, int(get_host_attributes(self.initial_hostnames[i])['architecture']['nb_cores'])*2):
                 hf.write(self.initial_hostnames[i]+"\n")
         hf.close()
-        return restart_colmet
 
     def update_colmet(self, new_sampling_period, new_metrics):
         command_update = "python ~/colmet-rs/configure.py {} {}".format(new_sampling_period, new_metrics)
@@ -142,9 +138,10 @@ class Colmet_bench(Engine):
         #install_open_mpi(self.hostnames)
         install_npb(self.initial_hostnames)
         install_colmet(self.collector_hostname, self.initial_hostnames)
+        #install_colmet(self.collector_hostname, nodes)
         self.update_hostnames(args.number_nodes-1)
-        #colmet_args=" --enable-perfhw"
-        #self.start_colmet("", colmet_args)
+        colmet_args=" --enable-perfhw"
+        self.start_colmet("", colmet_args)
 
     def clean_bench(self):
         oardel(self.jobs)
@@ -159,6 +156,7 @@ class Colmet_bench(Engine):
         self.params={}
         self.params['metrics']=p[1]
         self.params['sampling_period']=p[2]
+        #self.params['sampling_period']=p[1]
         self.params['mpi_root_host']=self.hostnames[0]
 
     def run_xp(self, uniform_parameters, parameters):
@@ -168,20 +166,22 @@ class Colmet_bench(Engine):
         bench_bin_path = "~/.nix-profile/bin/"
         mpi_executable_name = bench_bin_path + uniform_parameters['bench_name'] + "." + uniform_parameters['bench_class'] + "." + uniform_parameters['bench_type']
 
-        #self.update_colmet(self.params['sampling_period'], self.params['metrics'])
-        if self.colmet_launched == True:
+        self.update_colmet(self.params['sampling_period'], self.params['metrics'])
+        """if self.colmet_launched == True:
             self.kill_colmet()
-        self.start_colmet("--enable-stdout-backend -s {}".format(self.params['sampling_period']), "-s {}".format(self.params['sampling_period'])) 
+            print("killing colmet")
+        self.start_colmet("--enable-stdout-backend -s {}".format(self.params['sampling_period']), "-s {}".format(self.params['sampling_period'])) """
 
         bench_command = "mpirun -machinefile {}/nodefile -mca mtl psm2 -mca pml ^ucx,ofi -mca btl ^ofi,openib ".format(os.getcwd()) + mpi_executable_name
     
-        p = SshProcess(bench_command, self.params['mpi_root_host']).run(timeout=250)
+        p = SshProcess(bench_command, self.params['mpi_root_host']).run(timeout=300)
         p.wait()
         return parameters+";"+parse_output(p.stdout)+"\n"
 
 if __name__ == "__main__":
     starttime=time.time()
-    approx_time_expe_mins=1
+    approx_time_expe_mins=5
+    approx_time_setup=20
     args = ArgsParser.get_args()
     plan=experiment_plan_generator("expe_4.yml")
     #args.number_nodes=plan.get_max_nb_nodes()+1
@@ -199,7 +199,7 @@ if __name__ == "__main__":
     f.write(" ".join(list(str(uniform_parameters.values())))+"\n")
     f.write("repetition;metrics;sampling_period;time")
     bench = Colmet_bench()
-    bench.prepare_bench(args, format_walltime(plan.get_nb_remaining()*approx_time_expe_mins))
+    bench.prepare_bench(args, format_walltime(approx_time_setup + plan.get_nb_remaining() * approx_time_expe_mins))
     
     while plan.get_nb_remaining() > 0:
         print("Remaining : "+str(plan.get_percentage_remaining())+"%")
